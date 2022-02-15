c4c0c4745f6f997f577ce3971d9a82c54885233a
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-02-15 21:24:58 +0600

    db post added

--- index.js
+++ index.js
@@ -10,27 +10,29 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7oely.mongodb.net/${process.env.DB_Name}?retryWrites=true&w=majority`;
-
 
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect(err => {
-    const bookingCollection = client.db("emaJhonDB").collection("products");
-    
+    const products = client.db("emaJhonDB").collection("products");
 
+    app.post('/addProducts', (req, res) => {
+        const product = req.body;
+        products.insertOne(products)
+        .then(result=>{
+            console.log(result);
+        })
+    })
 
     console.log('db connected');
 });
 
 
-
 app.get('/', (req, res) => {
     res.send("Hlw NODE JS");
 })
-
 
 // app.listen(port);
 app.listen(port, () => {
