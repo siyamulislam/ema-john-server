79e968bc7df61dbf149bf9dc61dce49269672ce2
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-02-16 21:56:25 +0600

    formatted line

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7oely.mongodb.net/${process.env.DB_Name}?retryWrites=true&w=majority`;
 
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect(err => {
@@ -21,10 +20,10 @@ client.connect(err => {
     app.post('/addProducts', (req, res) => {
         const products = req.body;
         productsCollections.insertMany(products)
-        .then(result=>{
-            console.log(result);
-            res.send(result.acknowledged)
-        })
+            .then(result => {
+                console.log(result);
+                res.send(result.acknowledged)
+            })
         // productsCollections.insertOne(product)
         // .then(result=>{
         //    res.send(result.acknowledged)
@@ -32,30 +31,30 @@ client.connect(err => {
     })
     app.get('/products', (req, res) => {
         productsCollections.find({}).limit(20)
-        .toArray((err,documents)=>{
-            res.send(documents)
-        })
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
     })
     app.get('/product/:key', (req, res) => {
-        productsCollections.find({key:req.params.key})
-        .toArray((err,documents)=>{
-            res.send(documents[0])
-        })
+        productsCollections.find({ key: req.params.key })
+            .toArray((err, documents) => {
+                res.send(documents[0])
+            })
     })
     app.post('/productsByKey', (req, res) => {
-        const productKeys = req.body;  
-        productsCollections.find({key:{$in:productKeys}})
-        .toArray((err,documents)=>{
-            res.send(documents); 
-        })  
+        const productKeys = req.body;
+        productsCollections.find({ key: { $in: productKeys } })
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
     })
     app.post('/addOrder', (req, res) => {
         const order = req.body;
         ordersCollections.insertOne(order)
-        .then(result=>{
-            console.log(result);
-            res.send(result.acknowledged)
-        })
+            .then(result => {
+                console.log(result);
+                res.send(result.acknowledged)
+            })
     })
 
     console.log('db connected');
